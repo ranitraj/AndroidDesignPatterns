002aaa260334ce137dec4803675cbb89085c687d
Ranit Raj Ganguly <ranitrajganguly@gmail.com>
2020-07-10 04:23:11 +0530

    Adding Material Alert Dialog in MainActivityViewImpl

--- app/src/main/java/com/ranit/android/lexicon/LexiconApplication.kt
+++ app/src/main/java/com/ranit/android/lexicon/LexiconApplication.kt
@@ -1,18 +1,12 @@
 package com.ranit.android.lexicon
 
 import android.app.Application
+import android.content.Context
 import com.ranit.android.lexicon.model.db.WordDbOperations
 
 class LexiconApplication : Application() {
-    private lateinit var wordDbOperationsInstance: WordDbOperations
-
     @Override
     override fun onCreate() {
         super.onCreate()
-        wordDbOperationsInstance = WordDbOperations.getWordDbOperationsInstance(this)
-    }
-
-    fun getWordDbOperation() : WordDbOperations {
-        return wordDbOperationsInstance
     }
 }
--- app/src/main/java/com/ranit/android/lexicon/view/MainActivityViewImpl.kt
+++ app/src/main/java/com/ranit/android/lexicon/view/MainActivityViewImpl.kt
@@ -1,37 +1,38 @@
 package com.ranit.android.lexicon.view
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
-import com.ranit.android.lexicon.LexiconApplication
 import com.ranit.android.lexicon.R
 import com.ranit.android.lexicon.controller.MainActivityController
 import com.ranit.android.lexicon.model.ModelImpl
+import com.ranit.android.lexicon.model.db.WordDbOperations
 import com.ranit.android.lexicon.model.wordPojo.Word
 
-class MainActivityViewImpl constructor(context: Context, viewGroup : ViewGroup)
+class MainActivityViewImpl(context: Context, viewGroup: ViewGroup?)
     : MainActivityView {
 
-    private val applicationInstance : LexiconApplication = LexiconApplication()
-
     var rootView: View = LayoutInflater.from(context).inflate(R.layout.activity_main, viewGroup)
-    var modelImpl: ModelImpl = ModelImpl(applicationInstance.getWordDbOperation())
+    var modelImpl: ModelImpl = ModelImpl(WordDbOperations.getWordDbOperationsInstance(context.applicationContext))
     var mainActivityController: MainActivityController = MainActivityController(modelImpl,
         this)
 
     lateinit var recyclerView: RecyclerView
     lateinit var floatingActionButton: FloatingActionButton
+    var addWordDialog : MaterialAlertDialogBuilder = MaterialAlertDialogBuilder(context)
 
     override fun initView() {
         recyclerView = rootView.findViewById(R.id.recycler_view)
         floatingActionButton = rootView.findViewById(R.id.floating_action_button)
 
         floatingActionButton.setOnClickListener(View.OnClickListener {
-            mainActivityController.launchDialogOnFloatingButtonClicked()
+            showAddWordDialog()
         })
     }
 
@@ -51,5 +52,11 @@ class MainActivityViewImpl constructor(context: Context, viewGroup : ViewGroup)
         if (listOfWords.isNotEmpty()) {
             mainActivityController.getData()
         }
+    }
+
+    private fun showAddWordDialog() {
+        addWordDialog.setTitle("Add new word")
+            .setMessage("Add a word along with a brief description")
+            .show()
     }
 }
