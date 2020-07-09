797912ed365b127698a961c6cab373a317982fbb
Ranit Raj Ganguly <ranitrajganguly@Ranits-MBP.Dlink>
2020-07-09 23:32:40 +0530

    Added function declarations in MainActivityController

--- app/src/main/java/com/ranit/android/lexicon/controller/MainActivityController.kt
+++ app/src/main/java/com/ranit/android/lexicon/controller/MainActivityController.kt
@@ -1,4 +1,34 @@
 package com.ranit.android.lexicon.controller
 
+import com.ranit.android.lexicon.LexiconApplication
+import com.ranit.android.lexicon.model.ModelImpl
+
 class MainActivityController {
+    var application : LexiconApplication = LexiconApplication()
+    var modelImpl : ModelImpl
+
+    init {
+        modelImpl = ModelImpl(application.getWordDbOperation())
+    }
+
+    // Operations present in MainActivity
+    fun onFloatingActionButtonClicked() {
+
+    }
+
+    fun onAddButtonClicked() {
+
+    }
+
+    fun onCancelButtonClicked() {
+
+    }
+
+    fun onItemSelected() {
+
+    }
+
+    fun showErrorMessage(message : String) {
+
+    }
 }
