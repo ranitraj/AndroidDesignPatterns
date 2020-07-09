f1eb2ebee61cd0c5a5127fdc8b6a709ab55a897e
Ranit Raj Ganguly <ranitrajganguly@gmail.com>
2020-07-10 04:24:15 +0530

    Linking MainActivityViewImpl with MainActivity

--- app/src/main/java/com/ranit/android/lexicon/MainActivity.kt
+++ app/src/main/java/com/ranit/android/lexicon/MainActivity.kt
@@ -2,10 +2,23 @@ package com.ranit.android.lexicon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import com.ranit.android.lexicon.view.MainActivityViewImpl
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mainActivityViewImpl : MainActivityViewImpl
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainActivityViewImpl = MainActivityViewImpl(this, null)
+
+        setContentView(mainActivityViewImpl.rootView)
+        mainActivityViewImpl.initView()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mainActivityViewImpl.bindDataToView()
     }
 }
